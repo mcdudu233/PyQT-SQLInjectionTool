3f45d7e420afcfe7477aed86399c2f0bea0e8e89
桃源道不清 <xubiaoyu040827@outlook.com>
2025-05-17 19:26:20 +0800

    添加手动注入以及自动注入界面的组件调用接口

--- ui/main.py
+++ ui/main.py
@@ -230,6 +230,60 @@ class MainWindow(QMainWindow):
         if event.buttons() == Qt.RightButton:
             print('Mouse click: RIGHT CLICK')
 
+    #手动注入界面组件调用接口（注释后面为组件类型+组件名）
+    #地址设置 lineedit address
+    def setAddress(self):pass
+
+    #超时设置 lineedit overtime
+    def setOvertime(self):pass
+
+    #获取注入类型 comboBox injectionTyoe
+    def setStyleSheetInjectionType(self):pass
+
+    #线程设置 lineedit thread
+    def setThread(self):pass
+
+    #端口设置 lineedit portID
+    def setPortID(self):pass
+
+    #SSL是否选择 checkbox ssl
+    def ifSSL(self):pass
+
+    #编码设置 checkbox coding
+    def setCoding(self):pass
+
+    #数据库类型设置 checkbox databaseType
+    def setDatabase(self):pass
+
+    #重试次数设置 lineedit retry
+    def setRetryTimes(self):pass
+
+    #识别注入 button btn_startInjection
+    def startInjection(self):pass
+
+    #到处配置 button btn_exportConfiguration
+    def exportConfiguration(self):pass
+
+    #手动注入界面信号槽设置
+    #识别注入绑定
+    widgets.btn_startInjection.clicked.connect(startInjection)
+    widgets.btn_exportConfigurton.clicked.connect(exportConfiguration)
+
+    #自动注入界面组件调用接口
+    #URL设置 lineedit url
+    def setURL(self):pass
+    #启动浏览 button browse
+    def startBrowing(self):pass
+    #显示网页 QWebEngineView webPage
+    def showWebPage(self,url):pass
+
+    #自动输入界面信号槽
+    widgets.browse.clicked.connect(startBrowing)
+
+    #数据中心
+
+
+
 
 def main():
     app = QApplication(sys.argv)
