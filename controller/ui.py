36522b66350d1bc3cb7bf9f18293cb0bdabafa68
dudu233 <1147793932@qq.com>
2025-05-20 15:40:03 +0800

    实现手工注入页面的功能

--- controller/ui.py
+++ controller/ui.py
@@ -1,3 +1,6 @@
+import datetime
+import html
+
 from PySide6.QtCore import QUrl, QTimer
 from PySide6.QtWidgets import QMessageBox, QListWidgetItem, QLineEdit, QWidget
 
@@ -115,7 +118,106 @@ class UIWidgetsFunctions:
 
     ### 识别注入 button btn_startInjection ###
     def startInjection(self):
-        pass
+        try:
+            self.ui.btn_startInjection.setDisabled(True)
+
+            if self.current_task_id is not None:
+                self.sqlmap.delete_task(self.current_task_id)
+            self.current_task_id = self.sqlmap.create_task()
+
+            # 额外的参数
+            kwargs = {}
+
+            # 预处理URL
+            url = self.ui.autoUrl.text()
+            if not url.startswith(("http://", "https://")):
+                if self.ui.https:
+                    url = "https://" + url
+                else:
+                    url = "http://" + url
+            self.ui.autoUrl.setText(url)
+
+            # 获取注入参数
+            if self.ui.paramList.count() == 0:
+                params = None
+            else:
+                params = []
+                for i in range(self.ui.paramList.count()):
+                    params.append(self.ui.paramList.item(i).text())
+                kwargs |= {"testParameter": ",".join(params)}
+
+            # 请求方式
+            if self.ui.requestMethod.currentText() == "GET":
+                if params is not None:
+                    url += "?"
+                    for key in params:
+                        url += key + "=xxx&"
+                    url = url[:len(url) - 1]
+                kwargs |= {"method": "GET"}
+            elif self.ui.requestMethod.currentText() == "POST":
+                if params is not None:
+                    tmp = ""
+                    for key in params:
+                        tmp += key + "=xxx&"
+                    tmp = tmp[:len(tmp) - 1]
+                    kwargs |= {"data": tmp}
+                kwargs |= {"method": "POST"}
+            else:
+                kwargs |= {"method": self.ui.requestMethod.currentText()}
+
+            # 数据库类型
+            if self.ui.databaseType.currentText() != "自动检测":
+                kwargs |= {"dbms": self.ui.databaseType.currentText()}
+
+            # 注入方式
+            if self.ui.injectionType.currentText() != "自动检测":
+                # B：Boolean-based blind（布尔盲注）
+                # E：Error-based（报错注入）
+                # U：Union query-based（联合注入）
+                # S：Stacked queries（堆叠注入）
+                # T：Time-based blind（时间盲注）
+                # Q：Inline queries（内联注入）
+                key = None
+                tmp = self.ui.injectionType.currentText()
+                if tmp == "布尔盲注":
+                    key = "B"
+                elif tmp == "报错注入":
+                    key = "E"
+                elif tmp == "联合注入":
+                    key = "U"
+                elif tmp == "堆叠注入":
+                    key = "S"
+                elif tmp == "时间盲注":
+                    key = "T"
+                elif tmp == "内联注入":
+                    key = "Q"
+                if key is not None:
+                    kwargs |= {"technique": key}
+
+            print(kwargs)
+            self.sqlmap.start_scan(self.current_task_id, url, **kwargs)
+
+            self.sqlmap.poll_scan_completion(self.current_task_id)
+            data, error = self.sqlmap.get_scan_data(self.current_task_id)
+
+            if len(data) == 0 and len(error) == 0:
+                QMessageBox.warning(self.main, "注入失败", "未检测到注入点，详细信息请查看日志！")
+                self.ui.btn_logCenter.click()
+            else:
+                for i in data:
+                    print(i)
+                for i in error:
+                    print(i)
+
+            logs = self.sqlmap.get_scan_log(self.current_task_id)
+            for log in logs:
+                self._add_log_color(log["message"], log["level"], log["time"])
+
+            self.ui.btn_startInjection.setDisabled(False)
+        except Exception as e:
+            self.ui.btn_startInjection.setDisabled(False)
+            QMessageBox.critical(self.main, "注入失败", "注入失败，请查看日志！原因：" + str(e))
+            self.ui.btn_logCenter.click()
 
     ### 设置请求方式 ### combox requestMethod
     def setRequestMethod(self):
@@ -228,3 +330,33 @@ class UIWidgetsFunctions:
     ### 数据包发包记录 tableWidget packetSendingRecord ###
     def showPacketSendingRecord(self):
         pass
+
+    ### 添加文本到日志 ###
+    def _add_log_color(self, txt, level="PLAIN", time=None):
+        # 设置最大行数
+        # len(self.ui.log.toPlainText().split("\n"))
+
+        # 对应的颜色
+        level_colors = {
+            'INFO': '#00CC00',  # 绿色
+            'WARNING': '#FF9900',  # 橙色
+            'CRITICAL': '#FF0000',  # 红色
+            'DEBUG': '#999999',  # 灰色
+            'ERROR': '#CC00FF'  # 紫色
+        }
+
+        escaped_msg = html.escape(txt)
+        # 获取对应等级的颜色
+        color = level_colors.get(level, '#000000')  # 默认黑色
+        # 获取时间
+        if time is None:
+            time = datetime.datetime.now().strftime("%H:%M:%S")
+        # 构建HTML格式字符串
+        html_str = f"""
+               <div style="margin:2px 0;">
+                   <span style="color:#666666;">[{time}]</span>
+                   <span style="color:{color}; font-weight:500;">[{level}]</span>
+                   <span style="color:{color};">{escaped_msg}</span>
+               </div>
+               """
+        self.ui.log.appendHtml(html_str)
